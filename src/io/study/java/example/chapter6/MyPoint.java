93155fd997d98ffdbcc85e6b0bde24db388721b9
KimPro-KR <kimjw7854@naver.com>
2020-05-17 18:16:18 +0900

    Example

--- src/io/study/java/example/chapter6/MyPoint.java
+++ src/io/study/java/example/chapter6/MyPoint.java
@@ -4,29 +4,29 @@ public class MyPoint {
     private int x;
     private int y;
 
-    public MyPoint(){
+    public MyPoint(){       //초기좌표를 (0.0)으로 한다.
         this.x = 0;
         this.y = 0;
     }
 
-    public MyPoint(int x, int y){
+    public MyPoint(int x, int y){       //초기좌표를 저장
         this. x = x;
         this. y = y;
     }
 
-    public void setX(int px){
+    public void setX(int px){       // x좌표를 저장
         this.x = px;
     }
 
-    public void setY(int py){
+    public void setY(int py){       //y좌표를
         this.y = py;
     }
 
-    public int getX() {
+    public int getX() {        //x좌표를 취득
         return this.x;
     }
 
-    public int getY() {
+    public int getY() {       //y좌표를 취득
         return this.y;
     }
 
