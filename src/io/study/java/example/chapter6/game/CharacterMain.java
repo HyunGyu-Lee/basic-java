242545dad921808732a6b915e18a4cfe096956af
KimPro-KR <kimjw7854@naver.com>
2020-05-23 20:26:22 +0900

    dd

--- src/io/study/java/example/chapter6/game/CharacterMain.java
+++ src/io/study/java/example/chapter6/game/CharacterMain.java
@@ -3,7 +3,7 @@ package io.study.java.example.chapter6.game;
 import java.util.Scanner;
 
 public class CharacterMain {
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
 
@@ -17,7 +17,7 @@ public class CharacterMain {
         System.out.println("무기 공격력을 입력 하세요.");
         int weaponAtkPoint = sc.nextInt();
         sc.nextLine();
-        CharacterWeapon characterWeapon = new CharacterWeapon(weaponName,weaponAtkPoint);
+        CharacterWeapon characterWeapon = new CharacterWeapon(weaponName, weaponAtkPoint);
 
         System.out.println("2. 방어구 설정");
         System.out.println("방어구 이름을 입력 하시오.");
@@ -26,7 +26,7 @@ public class CharacterMain {
         System.out.println("방어구 방어력을 입력 하시오.");
         int defenceDfPoint = sc.nextInt();
         sc.nextLine();
-        CharacterDefence characterDefence = new CharacterDefence(defenceName,defenceDfPoint);
+        CharacterDefence characterDefence = new CharacterDefence(defenceName, defenceDfPoint);
 
         System.out.println("3. 강화수치 설정");
         System.out.println("무기 강화수치를 입력하시오.(1강화당 공격력 20 증가)");
@@ -37,8 +37,7 @@ public class CharacterMain {
         System.out.println("방어구 강화수치를 입력하시오.(1강화당 방어력 5 증가)");
         int defenceUpgrade = sc.nextInt();
         sc.nextLine();
-
-
+        characterDefence.setDefenceUpgrade(defenceUpgrade);
 
 
         System.out.println("4. 캐릭터의 기본 스펙 설정");
@@ -52,23 +51,13 @@ public class CharacterMain {
         Character character = new Character(characterName, characterAtkPoint, characterDfPoint);
 
         System.out.println("5.캐릭터 기본정보 증가");
-        System.out.println("# 캐릭터 : "+character.getCharacterName());
-        System.out.println("- 무기 : "+(character.getCharacterAtkPoint()+"+"+weaponUpgrade)+characterWeapon.getWeaponName());
-        System.out.println("- 방어 : "+(character.getCharacterDfPoint()+"+"+defenceDfPoint)+characterDefence.getDefenceName());
+        System.out.println("# 캐릭터 : " + character.getCharacterName());
+        System.out.println("- 무기 : " + (character.getCharacterAtkPoint() + "+" + weaponUpgrade) + characterWeapon.getWeaponName());
+        System.out.println("- 방어 : " + (character.getCharacterDfPoint() + "+" + defenceDfPoint) + characterDefence.getDefenceName());
         System.out.println("------------------------------------------");
         System.out.println("# 스탯");
-        System.out.println("- 공격력  "+characterWeapon.getWpUpgrade()+("기본"+character.getCharacterAtkPoint()+"무기"+characterWeapon.getWpUpgrade()));
-        System.out.println("- 방력 : "+characterDefence.getDfUpgrade()+("기본"+character.getCharacterDfPoint()+"무기"+characterDefence.getDfUpgrade()));
-
-
-
-
-
-
-
-
-
-
+        System.out.println("- 공격력  " + characterWeapon.getWpUpgrade() + ("  기본" + character.getCharacterAtkPoint() + "  무기" + (characterWeapon.getWeaponUpgrade() * 20)));
+        System.out.println("- 방력 : " + characterDefence.getDfUpgrade() + ("  기본" + character.getCharacterDfPoint() + "  무기" + (characterDefence.getDefenceUpgrade() * 5)));
 
 
     }
