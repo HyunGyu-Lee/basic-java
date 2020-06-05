44fc40043ecf4d8e98719f4f49e2a3e6a485c2f2
HyunGyu-Lee <gusrb0808@naver.com>
2020-06-05 17:51:56 +0900

    피드백 코드 추가

---
+++ src/io/study/java/example/chapter6/coffeeRe/CoffeeMachineV2.java
@@ -0,0 +1,41 @@
+package io.study.java.example.chapter6.coffeeRe;
+
+/**
+ * @author dlgusrb0808@gmail.com
+ */
+public class CoffeeMachineV2 {
+	private int a;
+	private int b;
+	private int c;
+
+	public CoffeeMachineV2() {
+		this.a = 10;
+		this.b = 10;
+		this.c = 10;
+	}
+
+	// 커피를 만들어주는 메소드
+	// 접근제한자 반환형 메소드명(인자 = 매개변수 = 파라미터 = 아규먼트)
+	// 인자 = 커피를 만들때 필요한것
+	// 메소드명 = 커피를 만들라
+	// 반환형 = 커피 클래스
+
+	public Coffee makeCoffee(String coffeeMenu, int shot, int water) {
+		Coffee coffee = new Coffee(coffeeMenu, shot, water);
+
+		useMaterial(coffee);
+
+		return coffee;
+	}
+
+	public void useMaterial(Coffee coffee) {
+		switch (coffee.getCoffeeMenu()) {
+			case "A":
+				a = a - 1; break;
+			case "B":
+				b = b - 1; break;
+			case "C":
+				c = c - 1; break;
+		}
+	}
+}
