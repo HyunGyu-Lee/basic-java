ec4e9157eee4639c1b5eca9995ffdf965f4d7e25
KimPro-KR <kimjw7854@naver.com>
2020-07-23 19:33:24 +0900

    [#6] 오늘 공부한 내용 중 예제 작성 해보고 싶은 부분 해봤습니다. 추가적인 공부 진행하겠습니다

---
+++ src/io/study/java/examples/object/string/charAt/Person.java
@@ -0,0 +1,22 @@
+package io.study.java.examples.object.string.charAt;
+
+public class Person {
+    public static void main(String[] args) {
+
+        String ssn = "102948-4094933";
+
+        char sex = ssn.charAt(7);
+
+        switch (sex){
+            case '1' :
+            case '3' :
+                System.out.println("남자입니다.");
+                break;
+            case '2' :
+            case '4' :
+                System.out.println("여자입니다.");
+                break;
+        }
+
+    }
+}
