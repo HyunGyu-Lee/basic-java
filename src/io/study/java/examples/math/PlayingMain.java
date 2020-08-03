a97bf3421a735d3eb2a1f7b5e9b3758e5b3b1fbc
KimPro-KR <kimjw7854@naver.com>
2020-08-03 18:30:17 +0900

    [#6] 보완할게 있어서 수정해서 다시 올립니다.

--- src/io/study/java/examples/math/PlayingMain.java
+++ src/io/study/java/examples/math/PlayingMain.java
@@ -9,9 +9,21 @@ public class PlayingMain {
         System.out.println("묵찌빠 중 하나를 내세요.");
         String playGo = sc.nextLine();
 
+
+        for(int i=0;i<10;i++){
+            if(playGo.equals("묵")||playGo.equals("찌")||playGo.equals("빠")){
+                break;
+            }else{
+                System.out.println("다시 입력하세요.");
+                playGo=sc.nextLine();
+                continue;
+            }
+        }
+
+
         Playing playing = new Playing();
         playing.setNum();
-        System.out.println(playing.setNum());
+        System.out.println(playing.getPlay());
 
 
         switch(playGo){
