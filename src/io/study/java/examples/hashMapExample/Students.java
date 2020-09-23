520f6d268eb1252c80eebb0ea7d0b7b01a7d110d
KimPro-KR <kimjw7854@naver.com>
2020-09-23 20:02:08 +0900

    [#7] 수정예정

---
+++ src/io/study/java/examples/hashMapExample/Students.java
@@ -0,0 +1,21 @@
+package io.study.java.examples.hashMapExample;
+
+public class Students {
+    private String name;
+    private int age;
+
+    public Students(String name, int age){
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+}
---
+++ src/io/study/java/examples/hashMapExample/StudentsExample.java
@@ -0,0 +1,62 @@
+package io.study.java.examples.hashMapExample;
+import java.util.*;
+
+public class StudentsExample {
+    public static void main(String[] args) {
+
+        List<Students> array = new ArrayList();
+        Map<Integer, List<Students>> map = new HashMap();
+
+        Scanner sc = new Scanner(System.in);
+
+
+        int num = 0;
+
+        while (num <= 3) {
+            if (num == 3) {
+                System.out.println("종료");
+                break;
+            }
+            System.out.println("============");
+            System.out.println("1. 학생 입력");
+            System.out.println("2. 기록부 조회");
+            System.out.println("3. 종료");
+            System.out.println("============");
+            num = sc.nextInt();
+            switch (num) {
+                case 1:
+                    System.out.println("학생 번호를 입력하시오");
+                    int number = sc.nextInt();
+                    System.out.println("이름을 입력하시오");
+                    sc.nextLine();
+                    String name = sc.nextLine();
+                    System.out.println("나이를 입력하시오");
+                    int age = sc.nextInt();
+
+                    Students st = new Students(name, age);
+                    array.add(st);
+                    map.put(number, array);
+
+                    break;
+                case 2:
+                    System.out.println("학생 조회");
+                    if (map.size() == 0) {
+                        System.out.println("등록 데이터 없습니다.");
+                        break;
+                    } else {
+                        for(int i=0 ; i<map.size();i++){
+                            Students students = array.get(i);
+                        }
+                            for (int i=0;i<map.size();i++) {
+                                Map.Entry entry = (Map.Entry) map.entrySet();
+                                Students students = array.get(i);
+                                System.out.println(String.format("[학생번호]:%s,[학생이름]:%s,[학생나이]:%s",entry.getKey(),students.getName(),students.getAge()));
+                            }
+
+
+                        }
+
+                    }
+            }
+        }
+    }
